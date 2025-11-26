677c5d120f2ab13050d77ec18500117bcc7489ee
marcvi1a <marc.vilallimos@gmail.com>
2025-11-26 02:16:34 -0300

    New

--- app.js
+++ app.js
@@ -19,13 +19,13 @@ const bulletPoint6 = document.getElementById("bullet-point-6");
 
 const BULLET_TITLE_SAUNA = "Sauna";
 const BULLET_TITLE_ICE_BATH = "Gelo";
-const BULLET_POINT_1_SAUNA = "Temperatura entre 70 ºC e 100 ºC";
+const BULLET_POINT_1_SAUNA = "Entre 70 ºC e 100 ºC";
 const BULLET_POINT_1_ICE_BATH = "Água abaixo de 15 ºC";
-const BULLET_POINT_2_SAUNA = "Sessões curtas e regulares para aumentar longevidade";
+const BULLET_POINT_2_SAUNA = "Sessão no fim do dia para melhorar sono";
 const BULLET_POINT_2_ICE_BATH = "Min 1 min para aumentar adrenalina e dopamina";
 const BULLET_POINT_3_SAUNA = "Sessão após cardio para melhorar endurance";
 const BULLET_POINT_3_ICE_BATH = "Max 3 min de imersão completa até o pescoço";
-const BULLET_POINT_4_SAUNA = "Sessão no fim do dia para melhorar sono";
+const BULLET_POINT_4_SAUNA = "Sessões com regularidade para aumentar longevidade";
 const BULLET_POINT_4_ICE_BATH = "Até 10 min só pernas para recuperação muscular";
 const BULLET_POINT_5_SAUNA = "100+ min totais por semana com 4 a 7 sessões";
 const BULLET_POINT_5_ICE_BATH = "11 min totais por semana com 3 a 5 mergulhos";
